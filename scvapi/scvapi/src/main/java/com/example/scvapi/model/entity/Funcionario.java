8c54e9435eef7fc0f82ce89e1a628025c205f459
Josias <josiassalermojunior>
2025-04-15 08:19:43 -0300

    classe PessoaFisica e Funcionario

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/Funcionario.java
@@ -0,0 +1,10 @@
+package com.example.scvapi.model.entity;
+
+public class Funcionario {
+
+    private String especialidade;
+    private String registroConselho;
+    private String dataAdmissao;
+    private Double salario;
+
+}
