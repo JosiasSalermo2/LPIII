7aba39058358016a2357185a01a0506aa748b8d6
Josias <josiassalermojunior>
2025-04-15 07:52:06 -0300

    criação do projeto

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/ScvapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.scvapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScvapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScvapiApplication.class, args);
+	}
+
+}
---
+++ scvapi/scvapi/src/test/java/com/example/scvapi/ScvapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scvapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScvapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
