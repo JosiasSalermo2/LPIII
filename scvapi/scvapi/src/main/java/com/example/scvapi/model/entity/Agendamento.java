c0a07c1a25037f4df38ec6d2ae1f2ab28ad094f5
Josias <josiassalermojunior>
2025-04-15 09:34:01 -0300

    classe Comorbidades

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/Agendamento.java
@@ -0,0 +1,9 @@
+package com.example.scvapi.model.entity;
+
+public class Agendamento {
+
+    private String dataAgendamento;
+    private String horarioAgendamento;
+
+
+}
---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/Comorbidades.java
@@ -0,0 +1,7 @@
+package com.example.scvapi.model.entity;
+
+public class Comorbidades {
+
+    private String comorbidade;
+    private String descricao;
+}
