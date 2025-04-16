792869f40f54e726c2b08bdb2e5d8f3170fd8b86
Annefrance Costa <annefrance.costa@gmail.com>
2025-04-16 19:44:48 -0300

    Created Endereco class

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/Endereco.java
@@ -0,0 +1,11 @@
+package com.example.scvapi.model.entity;
+
+public class Endereco {
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String cep;
+}
