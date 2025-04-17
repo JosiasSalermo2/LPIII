a5f6bb0801ba662c6b8f91d9c9125a2d6b07dea3
Annefrance Costa <annefrance.costa@gmail.com>
2025-04-17 07:42:24 -0300

    Created Lote class

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/Lote.java
@@ -0,0 +1,8 @@
+package com.example.scvapi.model.entity;
+
+public class Lote {
+    private String dataValidade;
+    private String numeroLote;
+    private int numeroAmpola;
+    private int dosesAmpola;
+}
