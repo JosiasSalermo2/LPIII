2cdb19a78cc6b45862104cd04fdb09b927a90c93
Annefrance Costa <annefrance.costa@gmail.com>
2025-04-17 07:32:16 -0300

    Created Estoque class

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/Estoque.java
@@ -0,0 +1,9 @@
+package com.example.scvapi.model.entity;
+
+public class Estoque {
+    private String nome;
+    private String fabricante;
+    private int quantidadeMinima;
+    private int quantidadeMaxima;
+    private int pontoRessuprimento;
+}
