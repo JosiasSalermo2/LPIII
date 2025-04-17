15e030274eec464f154586b4b20c1f86178e8176
Annefrance Costa <annefrance.costa@gmail.com>
2025-04-17 07:45:15 -0300

    Created Vacina class

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/Vacina.java
@@ -0,0 +1,9 @@
+package com.example.scvapi.model.entity;
+
+public class Vacina {
+    private String vacina;
+    private String tipoVacina;
+    private String indicacao;
+    private String contraIndicacao;
+    private int dosesAmpola;
+}
