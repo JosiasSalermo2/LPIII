462caf56f5d0aeac1f84a296a7265d9235175faf
Josias <josiassalermojunior>
2025-04-23 22:50:14 -0300

    Add Entity class PessoaFisica

--- scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/PessoaFisica.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/PessoaFisica.java
@@ -1,6 +1,18 @@
 package com.example.scvapi.model.entity;
 
-public class PessoaFisica {
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PessoaFisica extends Pessoa{
+
 
     private String dataNascimento;
     private String cpf;
