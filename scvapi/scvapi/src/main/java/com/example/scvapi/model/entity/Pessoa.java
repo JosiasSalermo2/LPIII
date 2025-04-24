1be64e32470459d767ef429e8314b10c9db19ae1
Josias <josiassalermojunior>
2025-04-23 22:26:46 -0300

    Add Entity class Pessoa

--- scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/Pessoa.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/Pessoa.java
@@ -1,6 +1,22 @@
 package com.example.scvapi.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
 public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
+    private Long id;
 
     private String nome;
     private String email;
