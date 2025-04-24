24e0d51f52e3349412bbf2566719b311805dbda1
Josias <josiassalermojunior>
2025-04-24 10:11:38 -0300

    update Mapped and Id class PessoaFisica

--- scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/PessoaFisica.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/PessoaFisica.java
@@ -5,19 +5,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
-@Entity
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class PessoaFisica extends Pessoa{
+@MappedSuperclass
+public abstract class PessoaFisica {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String nome;
+    private String email;
     private String dataNascimento;
     private String cpf;
     private String rg;
-    private String nome;
-    private String email;
+
 
 }
