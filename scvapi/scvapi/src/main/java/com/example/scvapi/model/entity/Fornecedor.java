337c09e9e28cc8bc5770e996e294a32c8ddec17c
Josias <josiassalermojunior>
2025-04-23 23:46:49 -0300

    Modification class Fornecedor

--- scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/Fornecedor.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/Fornecedor.java
@@ -1,11 +1,17 @@
 package com.example.scvapi.model.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
 @Data
-
+@NoArgsConstructor
+@AllArgsConstructor
 public class Fornecedor extends PessoaJuridica {
+
+
 
 }
