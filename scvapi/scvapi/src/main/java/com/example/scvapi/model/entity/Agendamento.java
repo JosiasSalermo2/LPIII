69aa94414a04f8ef62b1de640731002cdc027aa1
Josias <josiassalermojunior>
2025-04-23 22:08:47 -0300

    Adds Entity class Cargo

--- scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/Agendamento.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/Agendamento.java
@@ -1,5 +1,23 @@
 package com.example.scvapi.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Agendamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String dataAgendamento;
     private String horarioAgendamento;
--- scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/Cargo.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/Cargo.java
@@ -1,6 +1,24 @@
 package com.example.scvapi.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cargo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String cargo;
     private String descricao;
