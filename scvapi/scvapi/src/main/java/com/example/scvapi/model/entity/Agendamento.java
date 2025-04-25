bd99fcdefb00e773d4c9a9b065c1f2a52bd5b612
Josias <josiassalermojunior>
2025-04-24 21:45:42 -0300

    ManyToOne class Agendamento

--- scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/Agendamento.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/model/entity/Agendamento.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -21,6 +18,13 @@ public class Agendamento {
 
     private String dataAgendamento;
     private String horarioAgendamento;
+
+
+    @ManyToOne
+    private Paciente paciente;
+
+    @ManyToOne
+    private Vacinacao vacinacao;
 
 
 }
