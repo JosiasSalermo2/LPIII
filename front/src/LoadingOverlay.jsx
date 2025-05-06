2f38825d04842b04d4d602b7d9b96a133d247a04
Josias <josiassalermojunior>
2025-05-06 08:15:11 -0300

    deleção da pasta front

--- front/src/LoadingOverlay.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import LoadingSkeleton from './LoadingSkeleton';
-import './LoadingOverlay.css';
-
-function LoadingOverlay({ loading }) {
-  return loading ? <div className="overlay">Carregando...</div> : null;
-}
-
-export default LoadingOverlay;
