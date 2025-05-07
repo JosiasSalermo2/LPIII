64837f744210a4d32b4fb7de4e7ac6bb73ae6207
Annefrance Costa <annefrance.costa@gmail.com>
2025-05-07 11:59:12 -0300

    created static method to generate VacinacaoDTO

--- scvapi/scvapi/src/main/java/com/example/scvapi/api/dto/VacinacaoDTO.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/api/dto/VacinacaoDTO.java
@@ -1,5 +1,8 @@
 package com.example.scvapi.api.dto;
+import com.example.scvapi.model.entity.Agendamento;
+import com.example.scvapi.model.entity.Vacinacao;
 import lombok.*;
+import org.modelmapper.ModelMapper;
 
 @Data
 @NoArgsConstructor
@@ -9,4 +12,12 @@ public class VacinacaoDTO {
     private String dataVacinacao;
     private Long pacienteId;
     private Long agendamentoId;
+
+    public static VacinacaoDTO create(Vacinacao vacinacao) {
+        ModelMapper modelMapper = new ModelMapper();
+        VacinacaoDTO dto = modelMapper.map(vacinacao, VacinacaoDTO.class);
+        dto.pacienteId = vacinacao.getPaciente().getId();
+        dto.agendamentoId = vacinacao.getAgendamento().getId();
+        return dto;
+    }
 }
