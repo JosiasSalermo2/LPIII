bf6e6c9f64792e3bad0c96a53bac7e54a062df8f
Annefrance Costa <annefrance.costa@gmail.com>
2025-05-07 11:53:39 -0300

    created static method to generate CompraDTO

--- scvapi/scvapi/src/main/java/com/example/scvapi/api/dto/CompraDTO.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/api/dto/CompraDTO.java
@@ -1,5 +1,8 @@
 package com.example.scvapi.api.dto;
+import com.example.scvapi.model.entity.Agendamento;
+import com.example.scvapi.model.entity.Compra;
 import lombok.*;
+import org.modelmapper.ModelMapper;
 
 @Data
 @NoArgsConstructor
@@ -9,5 +12,19 @@ public class CompraDTO {
     private double valor;
     private String dataCompra;
     private Long fornecedorId;
+    private String fornecedorCNPJ;
+    private String fornecedorNome;
+    private String fabricanteCNPJ;
     private Long fabricanteId;
+    private String fabricanteNome;
+
+    public static CompraDTO create(Compra compra) {
+        ModelMapper modelMapper = new ModelMapper();
+        CompraDTO dto = modelMapper.map(compra, CompraDTO.class);
+        dto.fornecedorCNPJ = compra.getFornecedor().getCnpj();
+        dto.fornecedorNome = compra.getFornecedor().getNomeFantasia();
+        dto.fabricanteCNPJ = compra.getFornecedor().getCnpj();
+        dto.fabricanteNome = compra.getFabricante().getNomeFantasia();
+        return dto;
+    }
 }
