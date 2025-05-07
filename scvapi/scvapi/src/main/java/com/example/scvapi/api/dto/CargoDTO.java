bf60f250dd3e25752d21ae6cd63b7579291bfa31
Annefrance Costa <annefrance.costa@gmail.com>
2025-05-07 11:52:18 -0300

    created static method to generate CargoDTO

--- scvapi/scvapi/src/main/java/com/example/scvapi/api/dto/CargoDTO.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/api/dto/CargoDTO.java
@@ -1,5 +1,7 @@
 package com.example.scvapi.api.dto;
+import com.example.scvapi.model.entity.Cargo;
 import lombok.*;
+import org.modelmapper.ModelMapper;
 
 @Data
 @NoArgsConstructor
@@ -9,4 +11,12 @@ public class CargoDTO {
     private String cargo;
     private String descricao;
     private Long funcionarioId;
+    private String funcionarioNome;
+
+    public static CargoDTO create(Cargo cargo) {
+        ModelMapper modelMapper = new ModelMapper();
+        CargoDTO dto = modelMapper.map(cargo, CargoDTO.class);
+        dto.funcionarioNome = cargo.getFuncionario().getNome();
+        return dto;
+    }
 }
