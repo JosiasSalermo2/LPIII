84809d59e886393020677d82b9a1b35acccff9bb
Annefrance Costa <annefrance.costa@gmail.com>
2025-05-07 11:56:49 -0300

    created static method to generate FuncionarioDTO

--- scvapi/scvapi/src/main/java/com/example/scvapi/api/dto/FuncionarioDTO.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/api/dto/FuncionarioDTO.java
@@ -1,5 +1,8 @@
 package com.example.scvapi.api.dto;
+import com.example.scvapi.model.entity.Fornecedor;
+import com.example.scvapi.model.entity.Funcionario;
 import lombok.*;
+import org.modelmapper.ModelMapper;
 
 @Data
 @NoArgsConstructor
@@ -10,4 +13,9 @@ public class FuncionarioDTO {
     private String dataNascimento;
     private Double salario;
 
+    public static FuncionarioDTO create(Funcionario funcionario) {
+        ModelMapper modelMapper = new ModelMapper();
+        FuncionarioDTO dto = modelMapper.map(funcionario, FuncionarioDTO.class);
+        return dto;
+    }
 }
