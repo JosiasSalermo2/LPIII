b0539242d40ae767fefe6a5083d3236024acf5b9
Annefrance Costa <annefrance.costa@gmail.com>
2025-05-08 08:00:38 -0300

    create EnderecoRepository

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/repository/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package com.example.scvapi.repository;
+
+import com.example.scvapi.model.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long>{
+}
