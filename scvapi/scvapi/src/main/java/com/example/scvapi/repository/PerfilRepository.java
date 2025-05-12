f818e531943c02876ff96299c278d6602e307025
Josias <josiassalermojunior>
2025-05-12 16:25:18 -0300

    PerfilRepository

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/repository/PerfilRepository.java
@@ -0,0 +1,6 @@
+package com.example.scvapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PerfilRepository extends JpaRepository<PerfilRepository, Long> {
+}
