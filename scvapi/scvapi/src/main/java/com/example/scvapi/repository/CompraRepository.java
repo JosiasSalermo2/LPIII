69d06d25c6a6cf53b854d6c04487b8911503df90
Annefrance Costa <annefrance.costa@gmail.com>
2025-05-12 18:31:42 -0300

    create CompraRepository

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/repository/CompraRepository.java
@@ -0,0 +1,7 @@
+package com.example.scvapi.repository;
+
+import com.example.scvapi.model.entity.Compra;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompraRepository extends JpaRepository<Compra, Long>{
+}
