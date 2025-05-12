c2cffd805e2eab4537a890c4b55e81c88c8b969c
Josias <josiassalermojunior>
2025-05-12 16:16:21 -0300

    ComorbidadesRepository

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/repository/ComorbinadesRepository.java
@@ -0,0 +1,7 @@
+package com.example.scvapi.repository;
+
+import com.example.scvapi.model.entity.Comorbidades;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ComorbinadesRepository extends JpaRepository<Comorbidades,Long> {
+}
