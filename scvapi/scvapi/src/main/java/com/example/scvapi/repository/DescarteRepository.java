dffa80c15ad71fcb562e032876b71ab5176ac8e2
Annefrance Costa <annefrance.costa@gmail.com>
2025-05-12 18:32:27 -0300

    create DescarteRepository

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/repository/DescarteRepository.java
@@ -0,0 +1,7 @@
+package com.example.scvapi.repository;
+
+import com.example.scvapi.model.entity.Descarte;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DescarteRepository extends JpaRepository<Descarte, Long>{
+}
