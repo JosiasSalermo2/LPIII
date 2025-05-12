07966b5bead860af67b0298bcff7c15be2d91f46
Annefrance Costa <annefrance.costa@gmail.com>
2025-05-12 18:34:49 -0300

    create TipoVacinaRepository

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/repository/TipoVacinaRepository.java
@@ -0,0 +1,7 @@
+package com.example.scvapi.repository;
+
+import com.example.scvapi.model.entity.TipoVacina;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoVacinaRepository extends JpaRepository<TipoVacina, Long>{
+}
