e91ecec53a82a94f24646094a22d305aeebfba7b
Annefrance Costa <annefrance.costa@gmail.com>
2025-05-12 18:33:23 -0300

    create FabricanteRepository

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/repository/FabricanteRepository.java
@@ -0,0 +1,7 @@
+package com.example.scvapi.repository;
+
+import com.example.scvapi.model.entity.Fabricante;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FabricanteRepository extends JpaRepository<Fabricante, Long>{
+}
