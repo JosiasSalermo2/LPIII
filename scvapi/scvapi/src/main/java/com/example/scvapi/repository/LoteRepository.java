dceaf83dc33e6f2c6df8502e8848b367f9d0991b
Annefrance Costa <annefrance.costa@gmail.com>
2025-05-12 18:34:09 -0300

    create LoteRepository

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/repository/LoteRepository.java
@@ -0,0 +1,8 @@
+package com.example.scvapi.repository;
+
+import com.example.scvapi.model.entity.Lote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoteRepository extends JpaRepository<Lote, Long>{
+}
+
