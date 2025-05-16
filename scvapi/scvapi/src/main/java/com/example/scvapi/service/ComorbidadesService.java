6c33b5183103ab7e2e78d4f99e2a86f94a67a45e
Annefrance Costa <annefrance.costa@gmail.com>
2025-05-16 10:52:26 -0300

    Created ComorbidadesService class

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/service/ComorbidadesService.java
@@ -0,0 +1,49 @@
+package com.example.scvapi.service;
+
+import com.example.scvapi.exception.RegraNegocioException;
+import com.example.scvapi.model.entity.Cargo;
+import com.example.scvapi.model.entity.Comorbidades;
+import com.example.scvapi.repository.ComorbidadesRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class ComorbidadesService 
+{
+    private ComorbidadesRepository repository;
+    public ComorbidadesService(ComorbidadesRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public List<Comorbidades> getComorbidades()
+    {
+        return repository.findAll();
+    }
+
+    public Optional<Comorbidades> getComorbidadesById(Long id)
+    {
+        return repository.findById(id);
+    }
+
+    @Transactional
+    public Comorbidades salvar(Comorbidades comorbidades)
+    {
+        validar(comorbidades);
+        return repository.save(comorbidades);
+    }
+
+    @Transactional
+    public void excluir(Comorbidades comorbidades)
+    {
+        Objects.requireNonNull(comorbidades.getId());
+        repository.delete(comorbidades);
+    }
+
+    private void validar(Comorbidades comorbidades)
+    {
+        //aqui é para validar os campos obrigatórios
+    }
+}
