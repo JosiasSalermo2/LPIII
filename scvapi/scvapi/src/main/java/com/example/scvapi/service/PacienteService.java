25c2a7c43272c38b1c43bbb3070655b0e139b3f4
Annefrance Costa <annefrance.costa@gmail.com>
2025-05-16 10:56:08 -0300

    Created PacienteService class

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/service/PacienteService.java
@@ -0,0 +1,59 @@
+package com.example.scvapi.service;
+
+import com.example.scvapi.exception.RegraNegocioException;
+import com.example.scvapi.model.entity.Paciente;
+import com.example.scvapi.repository.PacienteRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class PacienteService
+{
+    private PacienteRepository repository;
+    public PacienteService(PacienteRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public List<Paciente> getPaciente()
+    {
+        return repository.findAll();
+    }
+
+    public Optional<Paciente> getPacienteById(Long id)
+    {
+        return repository.findById(id);
+    }
+
+    @Transactional
+    public Paciente salvar(Paciente paciente)
+    {
+        validar(paciente);
+        return repository.save(paciente);
+    }
+
+    @Transactional
+    public void excluir(Paciente paciente)
+    {
+        Objects.requireNonNull(paciente.getId());
+        repository.delete(paciente);
+    }
+
+    private void validar(Paciente paciente)
+    {
+        if (paciente.getNome() == null || paciente.getNome().trim().equals(""))
+        {
+            throw new RegraNegocioException("Nome inválido");
+        }
+        if (paciente.getEmail() == null || paciente.getEmail().trim().equals(""))
+        {
+            throw new RegraNegocioException("E-mail inválido");
+        }
+        if (paciente.getCpf() == null || paciente.getCpf().trim().equals(""))
+        {
+            throw new RegraNegocioException("CPF inválido");
+        }
+    }
+}
