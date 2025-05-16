a44eef6c54a1a8ab34df8bb8918d8034fe3ba40e
Annefrance Costa <annefrance.costa@gmail.com>
2025-05-16 10:58:38 -0300

    fixing class name

--- scvapi/scvapi/src/main/java/com/example/scvapi/repository/ComorbidadesRepository.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/repository/ComorbidadesRepository.java
@@ -3,5 +3,5 @@ package com.example.scvapi.repository;
 import com.example.scvapi.model.entity.Comorbidades;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ComorbinadesRepository extends JpaRepository<Comorbidades,Long> {
+public interface ComorbidadesRepository extends JpaRepository<Comorbidades,Long> {
 }
