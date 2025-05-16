5806bfcf01655f0367c010204e6a254d786942ff
Annefrance Costa <annefrance.costa@gmail.com>
2025-05-16 10:54:25 -0300

    Created FabricanteService class

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/service/FabricanteService.java
@@ -0,0 +1,59 @@
+package com.example.scvapi.service;
+
+import com.example.scvapi.exception.RegraNegocioException;
+import com.example.scvapi.model.entity.Fabricante;
+import com.example.scvapi.repository.FabricanteRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class FabricanteService
+{
+    private FabricanteRepository repository;
+    public FabricanteService(FabricanteRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public List<Fabricante> getFabricante()
+    {
+        return repository.findAll();
+    }
+
+    public Optional<Fabricante> getFabricanteById(Long id)
+    {
+        return repository.findById(id);
+    }
+
+    @Transactional
+    public Fabricante salvar(Fabricante fabricante)
+    {
+        validar(fabricante);
+        return repository.save(fabricante);
+    }
+
+    @Transactional
+    public void excluir(Fabricante fabricante)
+    {
+        Objects.requireNonNull(fabricante.getId());
+        repository.delete(fabricante);
+    }
+
+    private void validar(Fabricante fabricante)
+    {
+        if (fabricante.getNomeFantasia() == null || fabricante.getNomeFantasia().trim().equals(""))
+        {
+            throw new RegraNegocioException("Nome inválido");
+        }
+        if (fabricante.getEmail() == null || fabricante.getEmail().trim().equals(""))
+        {
+            throw new RegraNegocioException("E-mail inválido");
+        }
+        if (fabricante.getCnpj() == null || fabricante.getCnpj().trim().equals(""))
+        {
+            throw new RegraNegocioException("CNPJ inválido");
+        }
+    }
+}
