f5c76beca8506ab4ef82e89b49cb6ca448cd7720
Annefrance Costa <annefrance.costa@gmail.com>
2025-05-16 10:52:46 -0300

    Created EnderecoService class

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/service/EnderecoService.java
@@ -0,0 +1,53 @@
+package com.example.scvapi.service;
+
+import com.example.scvapi.exception.RegraNegocioException;
+import com.example.scvapi.model.entity.Endereco;
+import com.example.scvapi.repository.EnderecoRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class EnderecoService
+{
+    private EnderecoRepository repository;
+    public EnderecoService(EnderecoRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public List<Endereco> getEndereco()
+    {
+        return repository.findAll();
+    }
+
+    public Optional<Endereco> getEnderecoById(Long id)
+    {
+        return repository.findById(id);
+    }
+
+    @Transactional
+    public Endereco salvar(Endereco endereco)
+    {
+        validar(endereco);
+        return repository.save(endereco);
+    }
+
+    @Transactional
+    public void excluir(Endereco endereco)
+    {
+        Objects.requireNonNull(endereco.getId());
+        repository.delete(endereco);
+    }
+
+    private void validar(Endereco endereco)
+    {
+        if (endereco.getCep() == null || endereco.getCep().trim().equals("")) {
+            throw new RegraNegocioException("CEP inválido");
+        }
+        if (endereco.getNumero() == null || endereco.getNumero().trim().equals("")) {
+            throw new RegraNegocioException("Número inválido");
+        }
+    }
+}
