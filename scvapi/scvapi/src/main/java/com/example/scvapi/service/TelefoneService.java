d577ae04fffd7a6a7dd7e1d7f242e17497368704
Annefrance Costa <annefrance.costa@gmail.com>
2025-05-16 10:56:27 -0300

    Created TelefoneService class

---
+++ scvapi/scvapi/src/main/java/com/example/scvapi/service/TelefoneService.java
@@ -0,0 +1,55 @@
+package com.example.scvapi.service;
+
+import com.example.scvapi.exception.RegraNegocioException;
+import com.example.scvapi.model.entity.Telefone;
+import com.example.scvapi.repository.TelefoneRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class TelefoneService
+{
+    private TelefoneRepository repository ;
+    public TelefoneService(TelefoneRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public List<Telefone> getTelefone()
+    {
+        return repository.findAll();
+    }
+
+    public Optional<Telefone> getTelefoneById(Long id)
+    {
+        return repository.findById(id);
+    }
+
+    @Transactional
+    public Telefone salvar(Telefone telefone)
+    {
+        validar(telefone);
+        return repository.save(telefone);
+    }
+
+    @Transactional
+    public void excluir(Telefone telefone)
+    {
+        Objects.requireNonNull(telefone.getId());
+        repository.delete(telefone);
+    }
+
+    private void validar(Telefone telefone)
+    {
+        if (telefone.getDdd() == null || telefone.getDdd().trim().equals(""))
+        {
+            throw new RegraNegocioException("DDD inválido");
+        }
+        if (telefone.getNumero() == null || telefone.getNumero().trim().equals(""))
+        {
+            throw new RegraNegocioException("Número inválido");
+        }
+    }
+}
