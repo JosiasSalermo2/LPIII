080174ae39107e8efe6f84987dfe614ce3854592
Josias <josiassalermojunior>
2025-05-30 19:53:29 -0300

    Get PacienteController

--- scvapi/scvapi/src/main/java/com/example/scvapi/api/controller/PacienteController.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/api/controller/PacienteController.java
@@ -4,17 +4,29 @@ import com.example.scvapi.api.dto.PacienteDTO;
 import com.example.scvapi.model.entity.Paciente;
 import com.example.scvapi.service.PacienteService;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ResolvedPointcutDefinition;
 import org.modelmapper.ModelMapper;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/paciente")
+@RequestMapping("/api/v1/pacientes")
 @RequiredArgsConstructor
 @CrossOrigin
 public class PacienteController {
     private final PacienteService pacienteService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity get(@PathVariable("id") Long id) {
+        Optional<Paciente> paciente = pacienteService.getPacienteById(id);
+        if (!paciente.isPresent()) {
+            return new ResponseEntity("Paciente não encontrado", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(paciente.map(PacienteDTO::create));
+    }
 
     public Paciente converter(PacienteDTO dto){
         ModelMapper modelMapper = new ModelMapper();
