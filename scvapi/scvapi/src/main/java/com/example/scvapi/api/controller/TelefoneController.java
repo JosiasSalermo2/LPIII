fe7b8ba4b265daeaae8c7d317f0c51a1705e000a
Josias <josiassalermojunior>
2025-05-30 19:58:39 -0300

    Get TelefoneController

--- scvapi/scvapi/src/main/java/com/example/scvapi/api/controller/TelefoneController.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/api/controller/TelefoneController.java
@@ -2,17 +2,30 @@ package com.example.scvapi.api.controller;
 
 import com.example.scvapi.api.dto.TelefoneDTO;
 import com.example.scvapi.model.entity.Telefone;
+import com.example.scvapi.service.TelefoneService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/telefone")
+@RequestMapping("/api/v1/telefones")
 @RequiredArgsConstructor
 @CrossOrigin
 public class TelefoneController {
+
+    private final TelefoneService telefoneService;
+    @GetMapping("/{id}")
+    public ResponseEntity get(@PathVariable("id") Long id) {
+        Optional<Telefone> telefone = telefoneService.getTelefoneById(id);
+        if (!telefone.isPresent()) {
+            return new ResponseEntity("Telefone não encontrado", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(telefone.map(TelefoneDTO::create));
+    }
 
     public Telefone converter(TelefoneDTO dto){
         ModelMapper modelMapper = new ModelMapper();
