1081ce7f247ce8654b756ab545032d49ad349f12
Josias <josiassalermojunior>
2025-06-10 10:32:56 -0300

    Gets PacienteController

--- scvapi/scvapi/src/main/java/com/example/scvapi/api/controller/FuncionarioController.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/api/controller/FuncionarioController.java
@@ -9,18 +9,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/funcionarios")
 @RequiredArgsConstructor
 @CrossOrigin
 public class FuncionarioController {
-    private final FuncionarioService funcionarioService;
+    private final FuncionarioService service;
+
+    @GetMapping()
+    public ResponseEntity get(){
+        List<Funcionario> funcionarios = service.getFuncionario();
+        return ResponseEntity.ok(funcionarios.stream().map(FuncionarioDTO::create).collect(Collectors.toList()));
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity get(@PathVariable("id") Long id) {
-        Optional<Funcionario> funcionario = funcionarioService.getFuncionarioById(id);
+        Optional<Funcionario> funcionario = service.getFuncionarioById(id);
         if (!funcionario.isPresent()) {
             return new ResponseEntity("Funcionário não encontrado", HttpStatus.NOT_FOUND);
         }
--- scvapi/scvapi/src/main/java/com/example/scvapi/api/controller/PacienteController.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/api/controller/PacienteController.java
@@ -10,18 +10,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/pacientes")
 @RequiredArgsConstructor
 @CrossOrigin
 public class PacienteController {
-    private final PacienteService pacienteService;
+    private final PacienteService service;
+
+    @GetMapping()
+    public ResponseEntity get(){
+        List<Paciente> pacientes = service.getPaciente();
+        return ResponseEntity.ok(pacientes.stream().map(PacienteDTO::create).collect(Collectors.toList()));
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity get(@PathVariable("id") Long id) {
-        Optional<Paciente> paciente = pacienteService.getPacienteById(id);
+        Optional<Paciente> paciente = service.getPacienteById(id);
         if (!paciente.isPresent()) {
             return new ResponseEntity("Paciente não encontrado", HttpStatus.NOT_FOUND);
         }
