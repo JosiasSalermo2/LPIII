365e7ad1b1c3b44cdf64b02d12fa9107616daf32
Josias <josiassalermojunior>
2025-06-10 10:42:15 -0300

    Gets UsuarioController

--- scvapi/scvapi/src/main/java/com/example/scvapi/api/controller/UsuarioController.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/api/controller/UsuarioController.java
@@ -11,19 +11,27 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/usuarios")
 @RequiredArgsConstructor
 @CrossOrigin
 public class UsuarioController {
-    private final UsuarioService usuarioService;
+    private final UsuarioService service;
     private final FuncionarioService funcionarioService;
+
+    @GetMapping()
+    public ResponseEntity get(){
+        List<Usuario> usuarios = service.getUsuario();
+        return ResponseEntity.ok(usuarios.stream().map(UsuarioDTO::create).collect(Collectors.toList()));
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity get(@PathVariable("id") Long id) {
-        Optional<Usuario> usuario = usuarioService.getUsuarioById(id);
+        Optional<Usuario> usuario = service.getUsuarioById(id);
         if (!usuario.isPresent()) {
             return new ResponseEntity("Usuário não encontrado", HttpStatus.NOT_FOUND);
         }
