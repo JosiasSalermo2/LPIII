1c0c3a2c0642dc99e95c5240b6a47509d41e7c65
Josias <josiassalermojunior>
2025-06-10 10:14:56 -0300

    Get ComorbidadesController

--- scvapi/scvapi/src/main/java/com/example/scvapi/api/controller/CargoController.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/api/controller/CargoController.java
@@ -11,19 +11,27 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/cargos")
 @RequiredArgsConstructor
 @CrossOrigin
 public class CargoController {
-    private final CargoService cargoService;
+    private final CargoService service;
     private final FuncionarioService funcionarioService;
+
+    @GetMapping()
+    public ResponseEntity get(){
+        List<Cargo> cargos = service.getCargo();
+        return ResponseEntity.ok(cargos.stream().map(CargoDTO::create).collect(Collectors.toList()));
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity get(@PathVariable("id") Long id) {
-        Optional<Cargo> cargo = cargoService.getCargoById(id);
+        Optional<Cargo> cargo = service.getCargoById(id);
         if (!cargo.isPresent()) {
             return new ResponseEntity("Cargo não encontrado", HttpStatus.NOT_FOUND);
         }
--- scvapi/scvapi/src/main/java/com/example/scvapi/api/controller/ComorbidadesController.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/api/controller/ComorbidadesController.java
@@ -9,18 +9,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/comorbidades")
 @RequiredArgsConstructor
 @CrossOrigin
 public class ComorbidadesController {
-    private final ComorbidadesService comorbidadesService;
+    private final ComorbidadesService service;
+
+    @GetMapping()
+    public ResponseEntity get(){
+        List<Comorbidades> comorbidades = service.getComorbidades();
+        return ResponseEntity.ok(comorbidades.stream().map(ComorbidadesDTO::create).collect(Collectors.toList()));
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity get(@PathVariable("id") Long id) {
-        Optional<Comorbidades> comorbidades = comorbidadesService.getComorbidadesById(id);
+        Optional<Comorbidades> comorbidades = service.getComorbidadesById(id);
         if (!comorbidades.isPresent()) {
             return new ResponseEntity("Comorbidade não encontrada", HttpStatus.NOT_FOUND);
         }
