ef2468d8b2711ed8c6dfa25d410c961f5b1af0b0
Josias <josiassalermojunior>
2025-06-24 04:06:13 -0300

    Put UsuarioController

--- scvapi/scvapi/src/main/java/com/example/scvapi/api/controller/UsuarioController.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/api/controller/UsuarioController.java
@@ -50,6 +50,21 @@ public class UsuarioController {
         }
     }
 
+    @PutMapping("{id}")
+    public ResponseEntity atualizar(@PathVariable("id") Long id, @RequestBody UsuarioDTO dto) {
+        if (!service.getUsuarioById(id).isPresent()) {
+            return new ResponseEntity("Usuário não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            Usuario usuario = converter(dto);
+            usuario.setId(id);
+            service.salvar(usuario);
+            return ResponseEntity.ok(usuario);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
     public Usuario converter(UsuarioDTO dto){
         ModelMapper modelMapper = new ModelMapper();
         Usuario usuario = modelMapper.map(dto, Usuario.class);
