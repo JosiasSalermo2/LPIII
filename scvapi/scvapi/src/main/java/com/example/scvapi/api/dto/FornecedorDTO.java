0c53da80bfc54edde0eb9f6a2de332d6e34f3261
Annefrance Costa <annefrance.costa@gmail.com>
2025-08-06 15:01:17 -0300

    FornecedorDTO - adicao de atributos de Endereco

--- scvapi/scvapi/src/main/java/com/example/scvapi/api/dto/FornecedorDTO.java
+++ scvapi/scvapi/src/main/java/com/example/scvapi/api/dto/FornecedorDTO.java
@@ -16,6 +16,14 @@ public class FornecedorDTO {
     private Long telefoneId;
     private String telefoneDDD;
     private String telefoneNumero;
+    private Long enderecoId;
+    private String enderecoLogradouro;
+    private String enderecoNumero;
+    private String enderecoComplemento;
+    private String enderecoBairro;
+    private String enderecoCidade;
+    private String enderecoUf;
+    private String enderecoCep;
 
     public static FornecedorDTO create(Fornecedor fornecedor) {
         ModelMapper modelMapper = new ModelMapper();
@@ -23,6 +31,14 @@ public class FornecedorDTO {
         dto.telefoneId = fornecedor.getTelefone().getId();
         dto.telefoneDDD = fornecedor.getTelefone().getDdd();
         dto.telefoneNumero = fornecedor.getTelefone().getNumero();
+        dto.enderecoId = fornecedor.getEndereco().getId();
+        dto.enderecoLogradouro = fornecedor.getEndereco().getLogradouro();
+        dto.enderecoNumero = fornecedor.getEndereco().getNumero();
+        dto.enderecoComplemento= fornecedor.getEndereco().getComplemento();
+        dto.enderecoBairro= fornecedor.getEndereco().getBairro();
+        dto.enderecoCidade= fornecedor.getEndereco().getCidade();
+        dto.enderecoUf= fornecedor.getEndereco().getUf();
+        dto.enderecoCep= fornecedor.getEndereco().getCep();
         return dto;
     }
 }
